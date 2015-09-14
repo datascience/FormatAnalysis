09a5789a2b3d713b7bf71895e382bf55fe2ceff9
Krešimir Đuretec <kresimir.duretec@gmail.com>
2015-09-14 23:19:07 +0200

    adding plot of one bass model with all influences pointed out

--- Analysis/bassModelExample.R
+++ Analysis/bassModelExample.R
@@ -14,10 +14,10 @@ yp <- p*m*(1-(1-exp(-(p+q)*x))/((q/p)*exp(-(p+q)*x)+1))
 yq <- y - yp
 plot(NULL, main ="Bass", xlim=c(0,50), ylim=c(0,max(y)+0.1*max(y)), 
      xaxt='n', yaxt='n', ann=FALSE, xaxs="i", yaxs="i")
-lines(x,y)
+lines(x,y,lwd=3)
 polygon(c(x,rev(x)), c(rep(0,length(x)),rev(yp)), col='gray85', border=NA)
-lines(x,yp)
-text(40,0.4, labels=expression(paste(frac(q,p),"=50")))
+lines(x,yp,lwd=3)
+text(40,0.4, labels=expression(paste(frac(q,p),"=50")), cex=2)
 #lines(x,yq)
 
 
@@ -31,10 +31,10 @@ yp <- p*m*(1-(1-exp(-(p+q)*x))/((q/p)*exp(-(p+q)*x)+1))
 yq <- y - yp
 plot(NULL, main ="Bass", xlim=c(0,10), ylim=c(0,max(y)+0.1*max(y)),
      xaxt='n', yaxt='n', ann=FALSE, xaxs="i", yaxs="i")
-lines(x,y)
+lines(x,y,lwd=3)
 polygon(c(x,rev(x)), c(rep(0,length(x)),rev(yp)), col='gray85', border=NA)
-lines(x,yp)
-text(8,2.1, labels=expression(paste(frac(q,p),"=9")))
+lines(x,yp,lwd=3)
+text(8,2.1, labels=expression(paste(frac(q,p),"=9")), cex=2)
 #lines(x,yq)
 
 p <- 0.0001
@@ -47,10 +47,10 @@ yp <- p*m*(1-(1-exp(-(p+q)*x))/((q/p)*exp(-(p+q)*x)+1))
 yq <- y - yp
 plot(NULL, main ="Bass", xlim=c(0,200), ylim=c(0,max(y)+0.1*max(y)),
      xaxt='n', yaxt='n', ann=FALSE, xaxs="i", yaxs="i")
-lines(x,y)
+lines(x,y,lwd=3)
 polygon(c(x,rev(x)), c(rep(0,length(x)),rev(yp)), col='gray85', border=NA)
-lines(x,yp)
-text(175,0.1, labels=expression(paste(frac(q,p),"=600")))
+lines(x,yp,lwd=3)
+text(175,0.1, labels=expression(paste(frac(q,p),"=600")), cex=2)
 #lines(x,yq)
 
 p <- 0.2
@@ -63,10 +63,34 @@ yp <- p*m*(1-(1-exp(-(p+q)*x))/((q/p)*exp(-(p+q)*x)+1))
 yq <- y - yp
 plot(NULL, main ="Bass", xlim=c(0,15), ylim=c(0,max(y)+0.1*max(y)),
      xaxt='n', yaxt='n', ann=FALSE, xaxs="i", yaxs="i")
-lines(x,y)
+lines(x,y,lwd=3)
 polygon(c(x,rev(x)), c(rep(0,length(x)),rev(yp)), col='gray85', border=NA)
-lines(x,yp)
-text(12,1.3, labels=expression(paste(frac(q,p),"=1.5")))
+lines(x,yp,lwd=3)
+text(12,1.3, labels=expression(paste(frac(q,p),"=1.5")), cex=2)
 #lines(x,yq)
+
+
+#only one figure 
+
+# p <- 0.1
+# q <- 0.9
+# m <- 10
+# x <- seq(0,10,len=200)
+# y <- m*((p+q)^2/p)*((exp(-(p+q)*x))/(((q/p)*exp(-(p+q)*x) + 1 )^2))
+# q1 <- 0
+# yp <- p*m*(1-(1-exp(-(p+q)*x))/((q/p)*exp(-(p+q)*x)+1))
+# yq <- y - yp
+# plot(NULL, main ="Bass", xlim=c(0,10), ylim=c(0,max(y)+0.1*max(y)),
+#      xaxt='n', yaxt='n', ann=FALSE, xaxs="i", yaxs="i")
+# lines(x,y, lwd=3)
+# polygon(c(x,rev(x)), c(rep(0,length(x)),rev(yp)), col='gray85', border=NA)
+# lines(x,yp, lty=3, col="red", lwd=3)
+# lines(x,yq, lty=2, col="blue", lwd=3)
+# text(4,2.7, labels="Bass curve", adj = c(0,0), cex = 1.5)
+# arrows(3.9,2.75,x[50]+0.1,y[50]+0.001,length=0.15,angle=30, lwd = 2)
+# text(6,0.5, labels="External influence\ninnovators (p = 0.1)", adj = c(0,0), cex = 1.5)
+# arrows(7.9,0.45,x[70]-0.5,yp[70]-0.01,length=0.15,angle=30, lwd = 2)
+# text(5.5,1.8, labels="Internal influence\nimitators (q = 0.9)", adj = c(0,0), cex = 1.5)
+# arrows(5.9,1.75,x[70]-0.5,yq[70]-0.01,length=0.15,angle=30, lwd = 2)
 
 dev.off()
